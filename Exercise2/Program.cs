ec591ff19fcdca8170c3fdaa30622d791b21d707
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-02 15:44:24 +0700

    Mendeklarasi array dengan ukuran 50

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -4,7 +4,8 @@ namespace Exercise2
 {
     class InsertionSort
     {
-
+        //Deklarasi array integer dengan ukuran 30 + 20 - 2 x 15 + 10 + 20
+        private int[] osi = new int[50];
 
         static void Main(string[] args)
         {
