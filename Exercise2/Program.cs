17aaa83e11b1a0b2c22b1eb4fcfb7cb4ea9d9638
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-02 16:48:44 +0700

    Membuat method untuk memasukkan input dari user

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -76,6 +76,32 @@ namespace Exercise2
         //Deklarasi variabel untuk menyimpan banyaknya data pada array
         private int n;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 50)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 50 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Masukkan elemen array");
+            Console.WriteLine("--------------------------");
+
+            //User memasukkan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + " > ");
+                string s1 = Console.ReadLine();
+                osi[i] = Int32.Parse(s1);
+            }
+        }
+
     }
 
     class Program
