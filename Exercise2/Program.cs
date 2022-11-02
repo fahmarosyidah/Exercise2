3b7c1316162fa2c4749925c5e9d009ffa8de076f
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-02 16:46:27 +0700

    Membuat class MergeSort dan mendeklarasikan variabel

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -68,6 +68,15 @@ namespace Exercise2
         }
     }
 
+    class MergeSort
+    {
+        //Deklarasi array integer dengan ukuran 30 + 20 - 2 x 15 + 10 + 20
+        private int[] osi = new int[50];
+        private int[] a = new int[50];
+        //Deklarasi variabel untuk menyimpan banyaknya data pada array
+        private int n;
+
+    }
 
     class Program
     { 
